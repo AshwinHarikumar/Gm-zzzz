f2973a2fb01830ee6b29354af07765eca37517e1
gouthamsanthosh-gsx <gouthamsanthosh.xyz@gmail.com>
2024-10-25 18:14:49 +0530

    Install node modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import ResponsiveAppBar from './AppBar'
 import Sensor from './sensor'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
